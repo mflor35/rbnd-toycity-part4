7a75e3b7222748ffa16f77f9f1bc910ae3573e5d
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-04-13 19:52:17 -0700

    Fill the products database with fake data.
    Signed-off-by: Miguel Flores Silverio <floresmigu3l@gmail.com>

--- data/seeds.rb
+++ data/seeds.rb
@@ -5,4 +5,9 @@ require 'faker'
 
 def db_seed
   # Your code goes here!
+  20.times do
+    Product.create(brand: Faker::Company.name,
+                   name: Faker::Commerce.product_name,
+                   price: Faker::Commerce.price)
+  end
 end
