ca2350d54d5548d58abe096aacbd65940fe8ea9f
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-05-09 08:51:17 -0700

    Refactor calculating average prices

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,10 +1,7 @@
 module Analyzable
   # Your code goes here!
   def average_price(list_items)
-    sum = 0.0
-    list_items.each do |item|
-      sum += item.price.to_f
-    end
+    sum = list_items.inject(0){ |total, item| total + item.price.to_f }
     (sum/list_items.length).round(2)
   end
 
